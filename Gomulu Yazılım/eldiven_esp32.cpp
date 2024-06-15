71df0f821214bcf127d82307493414a079715822
Doğukan Biçer <dogukanbicer52@gmail.com>
2024-06-15 11:53:18 +0300

    Update eldiven_esp32.cpp

--- Gomulu Yazılım/eldiven_esp32.cpp
+++ Gomulu Yazılım/eldiven_esp32.cpp
@@ -2,6 +2,10 @@
 #include <WiFiUdp.h>
 #include "BNO055_support.h"		//Contains the bridge code between the API and Arduino
 #include <Wire.h>
+#include <EEPROM.h>
+
+#define EEPROM_SIZE 96
+bool wifiConnected = false;
 
 const int led_g=18;
 const int led_b=5;
@@ -59,31 +63,22 @@ void setup() {
   Serial.begin(115200);
   Serial.println(".:Marel Arge:.");
 
-// WiFi ağa bağlan
-  Serial.println("WiFi'ye bağlanılıyor");
+  // WiFi ağa bağlan
+  EEPROM.begin(EEPROM_SIZE);
+  Serial.println("EEPROM okunmaya hazır");
 
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("Wifi konfigurasyon hatasi!");
-  }
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi'ye bağlanıldı");
-
-  // UDP sunucusunu başlat
-  if (udp.begin(localPort)) {
-    Serial.println("UDP sunucusu başlatıldı");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+  if (isEEPROMEmpty()) {
+    Serial.println("EEPROM boş, yeni kimlik bilgileri bekleniyor.");
+    waitForCredentials();  // EEPROM boşsa yeni kimlik bilgilerini bekle
   } else {
-    Serial.println("UDP sunucusu başlatılamadı");
-    while (1); // Durdur ve hata durumunda döngüye gir
+    Serial.println("EEPROM'dan SSID ve şifre okunuyor.");
+    // EEPROM'dan SSID ve şifreyi okuma işlemini yap
+    String ssid = readStringFromEEPROM(0);
+    String password = readStringFromEEPROM(32);
+
+    // WiFi bağlantısını kurma işlemini gerçekleştir
+    connectToWifi(ssid, password);
   }
-
-
 }
 
 void loop() {
@@ -134,7 +129,7 @@ void loop() {
     digitalWrite(led_r, toggle3);
     old_time3 = new_time;
 
-    IPAddress local_IP(192, 168, 144, 80);
+    IPAddress local_IP(192, 168, 11, 35);
         // İstemci adresini alın
     IPAddress remoteIp = udp.remoteIP();
 
@@ -156,4 +151,125 @@ void loop() {
       Serial.println(pwmValue);
     }
   }
+}
+
+
+void waitForCredentials() {
+  String input;
+  String ssid = "";
+  String password = "";
+  bool ssidReceived = false;
+
+  while (true) {
+    if (Serial.available()) {
+      char ch = (char)Serial.read();
+      input += ch;
+
+      if (ch == '\n') {
+        input.trim();  // Trim uygula
+        if (!ssidReceived) {
+          ssid = input;
+          Serial.println("SSID alindi: " + ssid);
+          ssidReceived = true;
+        } else {
+          password = input;
+          Serial.println("Sifre alindi: " + password);
+
+          // Eğer SSID ve şifre alındıysa EEPROM'a kaydet ve yeniden başlat
+          if (ssid.length() > 0 && password.length() > 0) {
+            writeStringToEEPROM(0, ssid);
+            writeStringToEEPROM(32, password);
+            EEPROM.commit();
+
+            // Cihazı yeniden başlat
+            ESP.restart();
+          }
+        }
+
+        // Giriş tamamlandığında input'u temizle
+        input = "";
+      }
+    }
+  }
+}
+
+// EEPROM'a string yazma fonksiyonu
+void writeStringToEEPROM(int addr, String data) {
+  int len = data.length();
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(addr + i, data[i]);
+  }
+  EEPROM.write(addr + len, '\0');  // String sonuna null karakter ekle
+}
+
+// EEPROM'dan string okuma fonksiyonu
+String readStringFromEEPROM(int addr) {
+  char data[32];
+  int len = 0;
+  unsigned char k;
+  k = EEPROM.read(addr);
+  while (k != '\0' && len < 32) {
+    k = EEPROM.read(addr + len);
+    data[len] = k;
+    len++;
+  }
+  data[len] = '\0';
+  return String(data);
+}
+
+void connectToWifi(const String &ssid, const String &password) {
+
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS,
+                   secondaryDNS)) {
+    Serial.println("Wifi konfigurasyon hatasi!");
+  }
+
+
+  if (ssid.length() > 0 && password.length() > 0) {
+    // WiFi'ye bağlanmayı dene
+    WiFi.begin(ssid.c_str(), password.c_str());
+    Serial.print("Wifi'e baglaniyor");
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+
+      // UART üzerinden yeni SSID ve şifre almayı kontrol et
+      if (Serial.available()) {
+        waitForCredentials();  // Yeni kimlik bilgilerini bekle
+      }
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("\nWifi'e baglandi");
+      wifiConnected = true;  // WiFi bağlantısı başarılı
+
+
+      // UDP sunucusunu başlat
+      if (udp.begin(localPort)) {
+        Serial.println("UDP sunucusu başlatıldı");
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+      } else {
+        Serial.println("UDP sunucusu başlatılamadı");
+        while (1)
+          ;  // Durdur ve hata durumunda döngüye gir
+      }
+    } else {
+      Serial.println("\nBaglanti basarisiz kimlik bilgileri bekleniyor.");
+      waitForCredentials();  // WiFi'ye bağlanamazsa yeni kimlik bilgilerini bekle
+    }
+  } else {
+    // EEPROM'da SSID veya şifre yoksa yeni kimlik bilgilerini bekle
+    waitForCredentials();
+  }
+}
+
+bool isEEPROMEmpty() {
+  for (int i = 0; i < EEPROM_SIZE; i++) {
+    if (EEPROM.read(i) != 255) {
+      return false;
+    }
+  }
+  return true;
 }
