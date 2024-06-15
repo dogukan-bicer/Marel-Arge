4b508c02034efc3e7ca7b97ea385a6184615f865
Doğukan Biçer <dogukanbicer52@gmail.com>
2024-06-15 11:53:55 +0300

    Update robotik_el_esp32.cpp

--- Gomulu Yazılım/robotik_el_esp32.cpp
+++ Gomulu Yazılım/robotik_el_esp32.cpp
@@ -1,24 +1,22 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
-
-
+#include <EEPROM.h>
 #include <Wire.h>
-#include "ADS1015-SOLDERED.h"
+#include <Adafruit_ADS1X15.h>
 
-// Declare an ADS1015 class instance.
-ADS1015 ADS;
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 
-uint32_t new_time, old_time,old_time2;
+uint32_t new_time, old_time, old_time2;
 boolean toggle = false;
 int analog;
 String Robotik_data;
-  // setting PWM properties
-const int freq     = 1000;
-const int ledChannel =  0;
-const int resolution =  8;
-const int ledPin     = 17;
+// setting PWM properties
+const int freq = 1000;
+const int ledChannel = 0;
+const int resolution = 8;
+const int ledPin = 17;
 
-const int parmak_1b  = 33;
+const int parmak_1b = 33;
 const int parmak_2is = 25;
 const int parmak_3or = 26;
 const int parmak_4yz = 27;
@@ -39,41 +37,43 @@ const int pwm_kanal_5 = 5;
 const int emg_adc_pin = 4;
 
 int emg_analog;
+int emg_analog2;
 
-int parmak_1b_pwm,parmak_2is_pwm,parmak_3or_pwm,
-parmak_4yz_pwm,parmak_5sr_pwm=0;
+int parmak_1b_pwm, parmak_2is_pwm, parmak_3or_pwm,
+    parmak_4yz_pwm, parmak_5sr_pwm = 0;
 
-int parmak_1b_analog,parmak_2is_analog,parmak_3or_analog,
-parmak_4yz_analog,parmak_5sr_analog=0;
+int parmak_1b_analog, parmak_2is_analog, parmak_3or_analog,
+    parmak_4yz_analog, parmak_5sr_analog = 0;
 
 char udp_paketi[20];  // Gelen UDP paketi için buffer
 
-const char* ssid = "marel_arge";
-const char* password = "test1234";
-int localPort = 1234; // UDP sunucusu portu
+const char *ssid = "marel_arge";
+const char *password = "test1234";
+int localPort = 1234;  // UDP sunucusu portu
 
 WiFiUDP udp;
 
-//statik ip
+// statik ip
 IPAddress local_IP(192, 168, 11, 34);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 0, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(8, 8, 4, 4); //optional
+IPAddress primaryDNS(8, 8, 8, 8);    // optional
+IPAddress secondaryDNS(8, 8, 4, 4);  // optional
+
+#define EEPROM_SIZE 96
+bool wifiConnected = false;
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
 
 void setup() {
-
-    Serial.println(__FILE__);
-    Serial.print("ADS1X15_LIB_VERSION: ");
-    Serial.println(ADS1X15_LIB_VERSION);
-
-    // setup ADS1015
-    ADS.begin();
-    ADS.setGain(1);     // 4.096 volt
-    ADS.setDataRate(7); // fast
-    ADS.setMode(0);     // continuous mode
-    ADS.readADC(0);     // kanal 0
-
+  if (!ads.begin()) {
+    Serial.println("Failed to initialize ADS.");
+    while (1)
+      ;
+  }
+  ads.setGain(GAIN_ONE);  // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  ads.setDataRate(RATE_ADS1115_860SPS);
 
   Serial.begin(115200);
   Serial.println(".:Marel Arge:.");
@@ -86,7 +86,7 @@ void setup() {
   pinMode(4, INPUT);
   pinMode(13, INPUT);
   pinMode(15, INPUT);
-  
+
   pinMode(poz_fb_1, INPUT);
   pinMode(poz_fb_2, INPUT);
   pinMode(poz_fb_3, INPUT);
@@ -101,129 +101,254 @@ void setup() {
   ledcSetup(pwm_kanal_5, freq, resolution);
   // kontrol edilecek kanalı GPIO'ya ekler
   ledcAttachPin(ledPin, ledChannel);
-  ledcAttachPin(parmak_1b,  pwm_kanal_1);
+  ledcAttachPin(parmak_1b, pwm_kanal_1);
   ledcAttachPin(parmak_2is, pwm_kanal_2);
   ledcAttachPin(parmak_3or, pwm_kanal_3);
   ledcAttachPin(parmak_4yz, pwm_kanal_4);
   ledcAttachPin(parmak_5sr, pwm_kanal_5);
- // WiFi ağa bağlan
-  Serial.println("WiFi'ye bağlanılıyor");
+  // WiFi ağa bağlan
+  EEPROM.begin(EEPROM_SIZE);
+  Serial.println("EEPROM okunmaya hazır");
 
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, 
-  secondaryDNS)) {
-    Serial.println("Wifi konfigurasyon hatasi!");
-  }
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi'ye bağlanıldı");
-
-  // UDP sunucusunu başlat
-  if (udp.begin(localPort)) {
-    Serial.println("UDP sunucusu başlatıldı");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+  if (isEEPROMEmpty()) {
+    Serial.println("EEPROM boş, yeni kimlik bilgileri bekleniyor.");
+    waitForCredentials();  // EEPROM boşsa yeni kimlik bilgilerini bekle
   } else {
-    Serial.println("UDP sunucusu başlatılamadı");
-    while (1); // Durdur ve hata durumunda döngüye gir
+    Serial.println("EEPROM'dan SSID ve şifre okunuyor.");
+    // EEPROM'dan SSID ve şifreyi okuma işlemini yap
+    String ssid = readStringFromEEPROM(0);
+    String password = readStringFromEEPROM(32);
+
+    // WiFi bağlantısını kurma işlemini gerçekleştir
+    connectToWifi(ssid, password);
   }
+
+  xTaskCreatePinnedToCore(
+      Task1code, /* Görev fonksiyonu. */
+      "Task1",   /* Görev adı. */
+      10000,     /* Yığın boyutu. */
+      NULL,      /* Parametre. */
+      1,         /* Görev önceliği. */
+      &Task1,    /* Görev tanıtıcısı. */
+      0);        /* Çekirdek. */
+
+  xTaskCreatePinnedToCore(
+      Task2code, /* Görev fonksiyonu. */
+      "Task2",   /* Görev adı. */
+      10000,     /* Yığın boyutu. */
+      NULL,      /* Parametre. */
+      1,         /* Görev önceliği. */
+      &Task2,    /* Görev tanıtıcısı. */
+      1);        /* Çekirdek. */
 }
 
 void loop() {
-  new_time = millis();
+  // Boş bırakıldı. Tüm işler taskler tarafından yapılacak.
+}
 
-  if (new_time - old_time > 700) {
-    toggle = !toggle;
-    digitalWrite(12, toggle);
-    old_time = new_time;
-    Serial.print("Emg pini=");
-    Serial.println(emg_analog);
-  }
-  if (new_time - old_time2 > 400) {
-    toggle = !toggle;
-    digitalWrite(17, toggle);
-    old_time2 = new_time;
-    // İstemci adresini alın
-    parmak_1b_analog=analogRead(poz_fb_1);
-    parmak_2is_analog=analogRead(poz_fb_2);
-    parmak_3or_analog=analogRead(poz_fb_3);
-    parmak_4yz_analog=analogRead(poz_fb_4);
-    parmak_5sr_analog=analogRead(poz_fb_5);
+void Task1code(void *parameter) {
+  for (;;) {
+    emg_analog = ads.readADC_SingleEnded(0);
+    emg_analog2 = ads.readADC_SingleEnded(1);
 
-    // İstemciye analog değeri gönderin
-    Robotik_data ="Ro=" + String(parmak_1b_analog) + "_" + String(parmak_2is_analog) + "_" 
-                  + String(parmak_3or_analog) + "_" + String(parmak_4yz_analog) + "_" + 
-                  String(parmak_5sr_analog) + "_" + String(emg_analog);
-    //Serial.println(Robotik_data);
-
-
-    IPAddress local_IP(192, 168, 11, 34);
-        // İstemci adresini alın
+    //udp sunucusuna gönder
     IPAddress remoteIp = udp.remoteIP();
-
-    int remotePort = udp.remotePort();
-
-    udp.beginPacket(remoteIp, remotePort);
-    udp.println(Robotik_data);
+    udp.beginPacket(remoteIp, localPort);
+    String test_Str = String(emg_analog) + ">" + String(emg_analog2);
+    udp.println(test_Str);
     udp.endPacket();
   }
+}
 
-  ///
-  ////
-  /////
-  emg_analog=	ADS.getValue();
-  // IPAddress remoteIp = udp.remoteIP();
-  // udp.beginPacket(remoteIp, localPort);
-  // String test_Str="Zaman:" + String(new_time) + "-" + "Emg Adc"+ String(emg_analog);
-  // udp.println(test_Str);
-  // udp.endPacket();
-  //////
-  ////
-  //
-
-// İstemci mesajını bekleyin
-  int packetSize = udp.parsePacket();
-  if (packetSize) {
-    Serial.println("Paket alındı");
-    // İstemciden PWM değerini alın
-    while (udp.available()) {
-    int len = udp.read(udp_paketi, 20);
-    if (len > 0) {
-      udp_paketi[len] = 0;
-      Serial.print("Gelen veri: ");
-      Serial.println(udp_paketi);
-      // Gelen stringi parçala
-      char *token = strtok(udp_paketi, "_");
-      if (token != NULL) {
-        parmak_1b_pwm = atoi(token);
-        token = strtok(NULL, "_");
-      }
-      if (token != NULL) {
-        parmak_2is_pwm = atoi(token);
-        token = strtok(NULL, "_");
-      }
-      if (token != NULL) {
-        parmak_3or_pwm = atoi(token);
-        token = strtok(NULL, "_");
-      }
-      if (token != NULL) {
-        parmak_4yz_pwm = atoi(token);
-        token = strtok(NULL, "_");
-      }
-      if (token != NULL) {
-        parmak_5sr_pwm = atoi(token);
-      }
+void Task2code(void *parameter) {
+  for (;;) {
+        new_time = millis();
+    if (new_time - old_time > 700) {
+      toggle = !toggle;
+      digitalWrite(12, toggle);
+      old_time = new_time;
+      Serial.print("kanal1=");
+      Serial.println(emg_analog);
+      Serial.print("kanal2=");
+      Serial.println(emg_analog2);
     }
-      ledcWrite(pwm_kanal_1,  parmak_1b_pwm);
-      ledcWrite(pwm_kanal_2, parmak_2is_pwm);
-      ledcWrite(pwm_kanal_3, parmak_3or_pwm);
-      ledcWrite(pwm_kanal_4, parmak_4yz_pwm);
-      ledcWrite(pwm_kanal_5, parmak_5sr_pwm);
-      ledcWrite(ledChannel, parmak_1b_pwm);
+    if (new_time - old_time2 > 400) {
+      toggle = !toggle;
+      digitalWrite(17, toggle);
+      old_time2 = new_time;
+      // İstemci adresini alın
+      parmak_1b_analog = analogRead(poz_fb_1);
+      parmak_2is_analog = analogRead(poz_fb_2);
+      parmak_3or_analog = analogRead(poz_fb_3);
+      parmak_4yz_analog = analogRead(poz_fb_4);
+      parmak_5sr_analog = analogRead(poz_fb_5);
+
+      // İstemciye analog değeri gönderin
+      Robotik_data = "Ro=" + String(parmak_1b_analog) + "_" + String(parmak_2is_analog) + "_" +
+       String(parmak_3or_analog) + "_" + String(parmak_4yz_analog) + "_" 
+       + String(parmak_5sr_analog) + "_" + String(emg_analog);
+      IPAddress local_IP(192, 168, 11, 34);
+       //   // İstemci adresini alın
+       // IPAddress remoteIp = udp.remoteIP();
+       // int remotePort = udp.remotePort();
+       // udp.beginPacket(remoteIp, remotePort);
+       // udp.println(Robotik_data);
+       // udp.endPacket();
+    }
+
+    // İstemci mesajını bekleyin
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+      Serial.println("Paket alındı");
+      // İstemciden PWM değerini alın
+      while (udp.available()) {
+        int len = udp.read(udp_paketi, 20);
+        if (len > 0) {
+          udp_paketi[len] = 0;
+          Serial.print("Gelen veri: ");
+          Serial.println(udp_paketi);
+          // Gelen stringi parçala
+          char *token = strtok(udp_paketi, "_");
+          if (token != NULL) {
+            parmak_1b_pwm = atoi(token);
+            token = strtok(NULL, "_");
+          }
+          if (token != NULL) {
+            parmak_2is_pwm = atoi(token);
+            token = strtok(NULL, "_");
+          }
+          if (token != NULL) {
+            parmak_3or_pwm = atoi(token);
+            token = strtok(NULL, "_");
+          }
+          if (token != NULL) {
+            parmak_4yz_pwm = atoi(token);
+            token = strtok(NULL, "_");
+          }
+          if (token != NULL) {
+            parmak_5sr_pwm = atoi(token);
+          }
+        }
+        ledcWrite(pwm_kanal_1, parmak_1b_pwm);
+        ledcWrite(pwm_kanal_2, parmak_2is_pwm);
+        ledcWrite(pwm_kanal_3, parmak_3or_pwm);
+        ledcWrite(pwm_kanal_4, parmak_4yz_pwm);
+        ledcWrite(pwm_kanal_5, parmak_5sr_pwm);
+        ledcWrite(ledChannel, parmak_1b_pwm);
+      }
     }
   }
-  
+}
+
+void waitForCredentials() {
+  String input;
+  String ssid = "";
+  String password = "";
+  bool ssidReceived = false;
+
+  while (true) {
+    if (Serial.available()) {
+      char ch = (char)Serial.read();
+      input += ch;
+
+      if (ch == '\n') {
+        input.trim();  // Trim uygula
+        if (!ssidReceived) {
+          ssid = input;
+          Serial.println("SSID alindi: " + ssid);
+          ssidReceived = true;
+        } else {
+          password = input;
+          Serial.println("Sifre alindi: " + password);
+
+          // Eğer SSID ve şifre alındıysa EEPROM'a kaydet ve yeniden başlat
+          if (ssid.length() > 0 && password.length() > 0) {
+            writeStringToEEPROM(0, ssid);
+            writeStringToEEPROM(32, password);
+            EEPROM.commit();
+            // Cihazı yeniden başlat
+            ESP.restart();
+          }
+        }
+        // Giriş tamamlandığında input'u temizle
+        input = "";
+      }
+    }
+  }
+}
+
+// EEPROM'a string yazma fonksiyonu
+void writeStringToEEPROM(int addr, String data) {
+  int len = data.length();
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(addr + i, data[i]);
+  }
+  EEPROM.write(addr + len, '\0');  // String sonuna null karakter ekle
+}
+// EEPROM'dan string okuma fonksiyonu
+String readStringFromEEPROM(int addr) {
+  char data[32];
+  int len = 0;
+  unsigned char k;
+  k = EEPROM.read(addr);
+  while (k != '\0' && len < 32) {
+    k = EEPROM.read(addr + len);
+    data[len] = k;
+    len++;
+  }
+  data[len] = '\0';
+  return String(data);
+}
+
+void connectToWifi(const String &ssid, const String &password) {
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("Wifi konfigurasyon hatasi!");
+  }
+
+  if (ssid.length() > 0 && password.length() > 0) {
+    // WiFi'ye bağlanmayı dene
+    WiFi.begin(ssid.c_str(), password.c_str());
+    Serial.print("Wifi'e baglaniyor");
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+
+      // UART üzerinden yeni SSID ve şifre almayı kontrol et
+      if (Serial.available()) {
+        waitForCredentials();  // Yeni kimlik bilgilerini bekle
+      }
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("\nWifi'e baglandi");
+      wifiConnected = true;  // WiFi bağlantısı başarılı
+
+      // UDP sunucusunu başlat
+      if (udp.begin(localPort)) {
+        Serial.println("UDP sunucusu başlatıldı");
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
+      } else {
+        Serial.println("UDP sunucusu başlatılamadı");
+        while (1)
+          ;  // Durdur ve hata durumunda döngüye gir
+      }
+    } else {
+      Serial.println("\nBaglanti basarisiz kimlik bilgileri bekleniyor.");
+      waitForCredentials();  // WiFi'ye bağlanamazsa yeni kimlik bilgilerini bekle
+    }
+  } else {
+    // EEPROM'da SSID veya şifre yoksa yeni kimlik bilgilerini bekle
+    waitForCredentials();
+  }
+}
+
+bool isEEPROMEmpty() {
+  for (int i = 0; i < EEPROM_SIZE; i++) {
+    if (EEPROM.read(i) != 255) {
+      return false;
+    }
+  }
+  return true;
 }
